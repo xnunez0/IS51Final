3be73c4229a614fca685929b9977bed68a3c0146
Xavier Nunez <xaviernunez83@gmail.com>
2021-05-13 17:13:34 -0700

    structured-english

---
+++ README.md
@@ -0,0 +1,1 @@
+IS51Final- Xavier Nunez
---
+++ is51final.py
@@ -0,0 +1,10 @@
+"""
+We are making a program that will do the following
+it will inspect a list of grades
+it will take the grades and seperate them
+it will seperate and display the number of grades
+it will seperate and display the average grade
+it will seperate and display the percentage of grades that are above the average grade
+
+"""
+
